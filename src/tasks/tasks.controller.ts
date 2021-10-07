daadb373bda0f691d8f3a5e2ef8efb83ecec8aef
Natanael Acero <nataacero1@gmail.com>
2021-10-06 21:17:24 -0500

    We added the delete service/controller and the update service/controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { title } from 'process';
 import { identity } from 'rxjs';
 import { CreateTaskDto } from './dto/create-task.dto';
-import { Task } from './task.model';
+import { GetTasksFilterDto } from './dto/get-task-filter.dto';
+import { Task, TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -10,8 +11,13 @@ export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getAllTasks(): Task[] {
-    return this.tasksService.getAllTasks();
+  getTasks(@Query() filterDto : GetTasksFilterDto): Task[] {
+    if (Object.keys(filterDto).length) {
+        return this.tasksService.getTaskWithFilters(filterDto);
+    }else{
+      return this.tasksService.getAllTasks();
+    }
+
   }
 
   @Get('/:id')
@@ -19,11 +25,24 @@ export class TasksController {
     return this.tasksService.getTaskById(id);
   }
 
-
   @Post()
   createTask(
      @Body () createTaskDto: CreateTaskDto
   ): Task {
     return this.tasksService.createTask(createTaskDto);
   }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id:string):void {
+   this.tasksService.deleteTask(id);
+  }
+
+  @Patch('/:id/status')
+  updateTask(
+    @Param('id') id:string,
+    @Body('status') status:TaskStatus
+  ):Task{
+    return this.tasksService.updateTask(id,status);
+  }
+
 }
